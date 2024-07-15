90e4187460893146f16eeb63b2317f573808f830
MemoryTn <16532149@aru.ac.th>
2024-07-16 01:50:05 +0700

    build9

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-document.querySelector('.nav-links').classList.toggle('active');
-})
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+const menuToggle = document.querySelector('.menu-toggle');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
