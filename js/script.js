c8de2f7821564803a60d9a4451665ff144ad1697
DarkNess1903 <nes0953575427@gmail.com>
2024-07-16 11:08:48 +0700

    แก้

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.getElementById('nav-links');
-const menuToggle = document.querySelector('.menu-toggle');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
