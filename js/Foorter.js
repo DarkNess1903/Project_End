94ed3e129c3911cfac1e476162ea0ce7ad3121da
DarkNess1903 <nes0953575427@gmail.com>
2024-07-16 11:58:12 +0700

    แก้

--- js/Foorter.js
+++
@@ -1,8 +0,0 @@
-function loadFooter() {
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
-}
