3c7f0e460685ed02df098b4f536d360c7a1bebeb
MemoryTn <16532149@aru.ac.th>
2024-07-24 23:02:54 +0700

    build15

--- js/navscript.js
+++
@@ -1,9 +0,0 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const navLinks = document.getElementById('nav-links');
-const menuToggle = document.querySelector('.menu-toggle');
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    document.getElementById('nav-links').classList.toggle('active');
-});
