b20c55d37706891c1b185d92eed61174024aaa3b
DarkNess1903 <nes0953575427@gmail.com>
2024-07-28 23:28:17 +0700

    หลังบ้าน

---
+++ admin/navbar.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet"> <!-- Add your custom styles here -->
+    <title>Product Edit</title>
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body id="page-top">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">Admin</div>
+            </a>
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            <!-- Nav Item - Ordering Information -->
+            <li class="nav-item">
+                <a class="nav-link" href="orderList.php">
+                    <i class="fas fa-fw fa-info-circle"></i>
+                    <span>Ordering Information</span></a>
+            </li>
+            <!-- Nav Item - Edit Product -->
+            <li class="nav-item">
+                <a class="nav-link" href="productEdit.php">
+                    <i class="fas fa-fw fa-edit"></i>
+                    <span>Edit Product</span></a>
+            </li>
+            <!-- Nav Item - Notification of News -->
+            <li class="nav-item">
+                <a class="nav-link" href="notification-of-news.html">
+                    <i class="fas fa-fw fa-bell"></i>
+                    <span>Notification of News</span></a>
+            </li>
+        </ul>
+        <!-- End of Sidebar -->
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <!-- Main Content Here -->
