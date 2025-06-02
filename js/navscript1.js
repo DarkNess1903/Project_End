04fe56a3c1fa5f07cd91b691b33ae5e90513b594
DarkNess1903 <199372370+AruNoppakun@users.noreply.github.com>
2025-06-03 03:27:47 +0700

    เริ่ม

--- js/navscript1.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-    
